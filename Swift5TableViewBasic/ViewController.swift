c3177b3f382909681bb97651b5f18090016a1fa2
Yoshiki <villager2525@gmail.com>
2020-11-27 23:53:05 +0900

    first commit

---
+++ Swift5TableViewBasic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift5TableViewBasic
+//
+//  Created by 西谷恭紀 on 2020/11/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
